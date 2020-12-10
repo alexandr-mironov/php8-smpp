d0223aeba58b91e53fc5885a9f43031eff230925
Alexandr Mironov <mironov.a.s@outlook.com>
2020-12-10 22:01:04 +0400

    update Address

--- src/Address.php
+++ src/Address.php
@@ -9,13 +9,6 @@ namespace smpp;
  */
 class Address
 {
-    /** @var int $ton - Type Of Number */
-    public  int     $ton;
-    /** @var int $npi - Numbering Plan Indicator */
-    public  int     $npi;
-    /** @var string $value */
-    public  string  $value;
-
     /**
      * Construct a new object of class Address
      *
@@ -25,9 +18,12 @@ class Address
      * @throws \InvalidArgumentException
      */
     public function __construct(
-        public string $value,
-        public int $ton = Smpp::TON_UNKNOWN,
-        public int $npi = Smpp::NPI_UNKNOWN
+        /** @var string $value */
+        public  string  $value,
+        /** @var int $ton - Type Of Number */
+        public  int     $ton = Smpp::TON_UNKNOWN,
+        /** @var int $npi - Numbering Plan Indicator */
+        public  int     $npi = Smpp::NPI_UNKNOWN
     )
     {
         // Address-Value field may contain 10 octets (12-length-type), see 3GPP TS 23.040 v 9.3.0 - section 9.1.2.5 page 46.
@@ -37,9 +33,5 @@ class Address
         if ($ton == Smpp::TON_INTERNATIONAL && $npi == Smpp::NPI_E164 && strlen($value) > 15) {
             throw new \InvalidArgumentException('E164 address may only contain 15 digits');
         }
-
-//        $this->value = (string) $value;
-//        $this->ton = $ton;
-//        $this->npi = $npi;
     }
 }
